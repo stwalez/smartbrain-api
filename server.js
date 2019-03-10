b7f33f02b846c99fae9863fe992cfbe479a72a28
Olawale <stwalez1@gmail.com>
2019-03-10 09:45:46 +0100

    corrected body parser module

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bodyParser = require('body-Parser');
+const bodyParser = require('body-parser');
 const cors = require('cors');
 const knex = require('knex');
 const bcrypt = require('bcrypt-nodejs');
